f8d728f98eb3b789ea2398fd8a6da9fc91918853
AirLinuxOfficial <85956786+AirLinuxOfficial@users.noreply.github.com>
2021-06-30 11:23:57 -0400

    Add files via upload

---
+++ src/api/scrapi.h
@@ -0,0 +1,6 @@
+#include "graphics.cpp"
+Graphics graphics;
+#include "terminal.cpp"
+Terminal terminal;
+#include "system.cpp"
+System ssys;
---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "./api/scrapi.h"
+
+using namespace std;
+
+int main(int argc, const char* argv[]) {
+    return 0;
+}
